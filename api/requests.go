dc111d25fc7264fd938e3c6c1b481b9ce6943a73
Fabio Colacio <fabio.colacio.dev@gmail.com>
2018-12-08 11:31:44 -0800

    Create MessageFetch

--- api/requests.go
+++ api/requests.go
@@ -24,12 +24,43 @@ var(
     }
 )
 
+func MessageFetch(jwt []byte, peer, since string) ([]map[string]string, error) {
+    url := host + "/get?peer=" + peer + "&since=" + since
+    request, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+        return nil, err
+    }
+    request.Header.Set("Session", string(jwt))
+    res, err := client.Do(request)
+    if err != nil {
+        return nil, err
+    }
+
+    defer res.Body.Close()
+    body, err := ioutil.ReadAll(res.Body)
+    if err != nil {
+        return nil, err
+    }
+
+    if res.StatusCode != 200 {
+        return nil, errors.New(string(body))
+    }
+
+    var messages []map[string]string
+    err = json.Unmarshal(body, &messages)
+    if err != nil {
+        return nil, err
+    }
+
+    return messages, err
+}
+
 func SetHost(newHost string) {
     host = newHost
 }
 
 func LookupUser(user string) error {
-    res, err := client.Get(host + "/lookup?user" + user)
+    res, err := client.Get(host + "/lookup?user=" + user)
     if err != nil {
         return err
     }
